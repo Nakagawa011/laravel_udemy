b872142799d83f00dcaf0fa1f4f4d559f7bcfad3
Nakagawa011 <nakagawa.mk@inazou.jp>
2022-10-05 09:02:36 +0900

    日本語化対応

--- routes/web.php
+++ routes/web.php
@@ -17,8 +17,12 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+Route::get('/test', [TestController, 'index']);
+
 Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
+
+
 
 require __DIR__.'/auth.php';
