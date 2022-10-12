bdfd34a790e0beca7a58e14e7f98f1a8c8e82b8b
Nakagawa011 <nakagawa.mk@inazou.jp>
2022-10-12 14:49:54 +0900

    tailwindcssの練習

--- resources/views/components/test-components/card.blade.php
+++ resources/views/components/test-components/card.blade.php
@@ -1,4 +1,4 @@
-<div class="w-60 border-2 shadow-md">
+<div class="w-40 border-2 shadow-md">
     <div>{{ $title }}</div>
     <div>画像</div>
     <div>{{ $content }}</div>
--- resources/views/test-pages/test1.blade.php
+++ resources/views/test-pages/test1.blade.php
@@ -1,6 +1,64 @@
+<?php
+$arr = [
+    [
+        'title' => 'タイトルA',
+        'content' => 'コンテントA',
+    ],
+    [
+        'title' => 'タイトルB',
+        'content' => 'コンテントB',
+    ],
+    [
+        'title' => 'タイトルC',
+        'content' => 'コンテントC',
+    ],
+    [
+        'title' => 'タイトルD',
+        'content' => 'コンテント',
+    ],
+    [
+        'title' => 'タイトルA',
+        'content' => 'コンテントA',
+    ],
+    [
+        'title' => 'タイトルB',
+        'content' => 'コンテントB',
+    ],
+    [
+        'title' => 'タイトルC',
+        'content' => 'コンテントC',
+    ],
+    [
+        'title' => 'タイトルD',
+        'content' => 'コンテント',
+    ],
+    [
+        'title' => 'タイトルA',
+        'content' => 'コンテントA',
+    ],
+    [
+        'title' => 'タイトルB',
+        'content' => 'コンテントB',
+    ],
+    [
+        'title' => 'タイトルC',
+        'content' => 'コンテントC',
+    ],
+    [
+        'title' => 'タイトルD',
+        'content' => 'コンテント',
+    ],
+];
+?>
+
 <x-test-components.app>
+    {{-- header --}}
     <x-slot name="header">
-        <p>test</p>
+        <p>teaast</p>
     </x-slot>
-    <x-test-components.card title="タイトル" content="コンテントです" />
+
+    {{-- contents --}}
+    @foreach ($arr as $value)
+        <x-test-components.card title="{{ $value['title'] }}" content="{{ $value['content'] }}" />
+    @endforeach
 </x-test-components.app>
