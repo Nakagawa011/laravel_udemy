47c5b4a8be7df947c78e1a79833825e06330ff29
Nakagawa011 <nakagawa.mk@inazou.jp>
2022-10-14 17:07:37 +0900

    コントローラーから値を渡す

--- app/Http/Controllers/TestController.php
+++ app/Http/Controllers/TestController.php
@@ -7,12 +7,15 @@ use App\Http\Controllers\Controller;
 
 class TestController extends Controller
 {
-    public function index1() {
-        $message = "メッセージ";
+    public function index1()
+    {
+        $message = "メッセージ(1)";
         return view('test-pages.test1', compact('message'));
     }
 
-    public function index2() {
-        return view('test-pages.test2');
+    public function index2()
+    {
+        $message = 'メッセージ(2)';
+        return view('test-pages.test2', compact('message'));
     }
 }
--- resources/views/test-pages/test2.blade.php
+++ resources/views/test-pages/test2.blade.php
@@ -5,7 +5,7 @@
     </x-slot>
     <div
         class="font-sans w-11/12 m-auto text-center text-gray-900 antialiased grid grid-cols-2 sm:grid-cols-4 xl:grid-cols-6">
-        <x-test-components.card title="test" content="test-content" message="test-message" />
+        <x-test-components.card title="test" content="test-content" :message=$message />
     </div>
 
 </x-test-components.app>
